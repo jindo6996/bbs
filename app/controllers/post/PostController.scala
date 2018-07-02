cf14f7c7c95fd2e1afef1b1971b10a510ad07dfd
Nguyen Tai Dai <nguyen_td@septeni-technology.jp>
2018-07-02 11:43:58 +0700

    update user story 2

--- app/controllers/post/PostController.scala
+++ app/controllers/post/PostController.scala
@@ -8,10 +8,7 @@ import scala.util.{ Failure, Success }
 @Singleton
 class PostController @Inject() (postDao: PostDao, cc: ControllerComponents) extends AbstractController(cc) {
   def listPost = Action {
-    postDao.getAll match {
-      case Success(posts) => Ok(views.html.post.listPost(posts))
-      case Failure(ex)    => InternalServerError("A server error occurred: " + ex.getMessage)
-    }
+    Ok(views.html.post.listPost(postDao.getAll.get))
   }
   def getPostByID(id: Int) = Action {
     Ok(views.html.post.viewPost(postDao.getByID(id).get))
